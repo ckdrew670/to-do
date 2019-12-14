32995875d6de68bb8f40f6a8a5a49ef03b74b22c
Charlotte Drew <ckdrew670@hotmail.com>
2019-12-14 17:11:39 +0000

    Set up new project and added React and Redux files. Edited App.js.

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Jumbotron } from "react-bootstrap";
+
+const App = () => (
+
+    <React.Fragment>
+        
+        <Jumbotron className="jumbotron mt-4 mb-0">
+            <h1>Things I Need To Do ...</h1>
+        </Jumbotron>
+        <Task/>
+        
+    </React.Fragment> 
+    
+);
+
+export default App;
